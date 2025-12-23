7339d7e7bf68844b0ed620e3efb8b0a7a75b6100
Simolite <drlite23@gmail.com>
2025-12-23 14:28:47 +0100

    naming edite

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arrach-Markhi-Zahoum</title>
+    <title>Arrach-Markhi-Zahoum-junior</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
